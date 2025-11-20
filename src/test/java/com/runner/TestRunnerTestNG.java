2cad071d3325fb4a6cadee74a9f6f9c0de163883
AtulShrivastava123 <ashrivastava14@gmail.com>
2025-11-20 14:08:16 +0530

    Added testng.xml

--- src/test/java/com/runner/TestRunnerTestNG.java
+++ src/test/java/com/runner/TestRunnerTestNG.java
@@ -3,7 +3,8 @@ package com.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features", glue = { "com.stepDefinitionTestNG",
+@CucumberOptions(features = "src/test/resources/features/OrangeHRMLogin.feature", 
+glue = { "com.stepDefinitionTestNG","com.hooks"
 		}, plugin = { "pretty", "html:target/cucumber-html-report" }, monochrome = true)
 
 public class TestRunnerTestNG extends AbstractTestNGCucumberTests {
